5e938e4ce17ed48d0bcd9704ff8881f14a1f2023
Tom Mann <tom.mann937@gmail.com>
2025-11-30 12:33:35 +0000

    add dataset handler

---
+++ src/lstm/dataset.py
@@ -0,0 +1,18 @@
+import torch 
+from torch.utils.data import Dataset
+
+class WindowedTimeSeries(Dataset):
+  def __init__(self, series, seq_length):
+    # Transpose as Pytroch expects shape: (time_steps, num_features)
+    self.series = torch.tensor(series, dtype=torch.float32).T
+    # Length of window
+    self.seq_length = seq_length
+
+  def __len__(self):
+    return self.series.shape[0] - self.seq_length
+  
+  def __getitem__(self, idx):
+    # Get a single window and it's target
+    x = self.series[idx : idx + self.seq_length]
+    y = self.series[idx + self.seq_length]
+    return x, y
