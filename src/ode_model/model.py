ed29fa218423ba4290cbcc9527638b19c845d783
Tom Mann <tom.mann937@gmail.com>
2025-12-03 23:25:39 +0000

    add custom RK4 algorithm for ODE model ~100* faster

--- src/ode_model/model.py
+++ src/ode_model/model.py
@@ -1,6 +1,6 @@
-from math import sin
 from scipy import integrate as inte
 import numpy as np
+from decimal import Decimal
 
 class LorenzModel:
   def __init__(self, c=30, sigma=10, beta=8/3, rho=28):
@@ -11,12 +11,12 @@ class LorenzModel:
 
   def deriv(self, t, state):
     x, y, z = state
-    dxdt = self.sigma * (y - self.c*sin(x/self.c))
-    dydt = self.c*sin(x/self.c) * (self.rho - z) -y
-    dzdt = self.c*sin(x/self.c) * y - self.beta * z
+    dxdt = self.sigma * (y - self.c*np.sin(x/self.c))
+    dydt = self.c*np.sin(x/self.c) * (self.rho - z) -y
+    dzdt = self.c*np.sin(x/self.c) * y - self.beta * z
     return [dxdt, dydt, dzdt]
   
-  def predict(self, y0, t_step, t_eval=None):
+  def predict_scipy(self, y0, t_step, t_eval=None):
 
     single_input = False
     if y0.ndim == 1:
@@ -35,12 +35,41 @@ class LorenzModel:
         return predictions[0]  
     return predictions
   
+  def vec_deriv(self, state):
+    x, y, z = state[..., 0], state[..., 1], state[..., 2]
+    dxdt = self.sigma * (y - self.c * np.sin(x / self.c))
+    dydt = self.c * np.sin(x / self.c) * (self.rho - z) - y
+    dzdt = self.c * np.sin(x / self.c) * y - self.beta * z
+    # Use axis=-1 as this handles the case when state is 1d nicely
+    return np.stack([dxdt, dydt, dzdt], axis=-1)
+  
+  def predict(self, y0, t_step, dt=0.01):
+    y = np.array(y0, copy=True)
+    # Change y to (N, 3) if only 1 input given
+    if y.ndim == 1:
+      y = y[np.newaxis, :]
+
+    if Decimal(str(t_step)) % Decimal(str(dt)) != Decimal("0.0"):
+      raise ValueError(f"Time step: {t_step}, is not a multiple of dt: {dt}")
+    
+    steps = int(t_step / dt)
+
+    for _ in range(steps):
+      k1 = self.vec_deriv(y)
+      k2 = self.vec_deriv(y + dt * k1 / 2)
+      k3 = self.vec_deriv(y + dt * k2 / 2)
+      k4 = self.vec_deriv(y + dt *k3)
+
+      y = y + dt*(k1 + 2*k2 + 2*k3 + k4) / 6
+
+    return y[0] if y0.ndim == 1 else y
+  
   def update_params(self, c=None, sigma=None, beta=None, rho=None):
-      if c is not None:
-          self.c = c
-      if sigma is not None:
-          self.sigma = sigma
-      if beta is not None:
-          self.beta = beta
-      if rho is not None:
-          self.rho = rho
+    if c is not None:
+      self.c = c
+    if sigma is not None:
+      self.sigma = sigma
+    if beta is not None:
+      self.beta = beta
+    if rho is not None:
+      self.rho = rho
