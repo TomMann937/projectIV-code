7f86a27040e95d13a484bd9e4b38a77bfc3c5fa3
Tom Mann <tom.mann937@gmail.com>
2025-12-10 18:09:56 +0000

    add handling for different input and target series to dataset handler

--- src/lstm/dataset.py
+++ src/lstm/dataset.py
@@ -2,9 +2,15 @@ import torch
 from torch.utils.data import Dataset
 
 class WindowedTimeSeries(Dataset):
-  def __init__(self, series, seq_length):
-    
-    self.series = torch.tensor(series, dtype=torch.float32)
+  def __init__(self, input_series, target_series=None, seq_length=10):
+
+    self. input_series = torch.tensor(input_series, dtype=torch.float32)
+
+    if target_series is None:
+      target_series = input_series
+
+    self.target_series = torch.tensor(target_series, dtype=torch.float32)
+
     # Length of window
     self.seq_length = seq_length
 
@@ -13,6 +19,6 @@ class WindowedTimeSeries(Dataset):
   
   def __getitem__(self, idx):
     # Get a single window and it's target
-    x = self.series[idx : idx + self.seq_length]
-    y = self.series[idx + self.seq_length]
+    x = self.input_series[idx : idx + self.seq_length]
+    y = self.target_series[idx + self.seq_length]
     return x, y
